ef1745ba4d8a90f25ee1368e938a5c9ed426f049
adenugbamichael <adenugbamicke@gmail.com>
2024-05-21 18:10:47 +0100

    Migrating the data layer

--- server.js
+++ server.js
@@ -7,63 +7,64 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
-
-
-app.get('/api/v1/whisper', async (req, res) => {
-  const whispers = await getAll()
-res.json(whispers)
-})
-
 app.get('/about', async (req, res) => {
   const whispers = await getAll()
   res.render('about', { whispers })
-  })
+})
 
-  app.get('/api/v1/whisper/:id', async (req, res) => {
-    const id = parseInt(req.params.id)
+app.get('/api/v1/whisper', async (req, res) => {
+  const whispers = await getAll()
+  res.json(whispers)
+})
+
+app.get('/api/v1/whisper/:id', async (req, res) => {
+  const id = req.params.id
+  const whisper = await getById(id)
+  if (!whisper) {
+    res.sendStatus(404)
+  } else {
+    res.json(whisper)
+  }
+})
+
+app.post('/api/v1/whisper', async (req, res) => {
+  const { message } = req.body
+  if (!message) {
+    res.sendStatus(400)
+  } else {
+    const whisper = await create(message)
+    res.status(201).json(whisper)
+  }
+})
+
+app.put('/api/v1/whisper/:id', async (req, res) => {
+  const { message } = req.body
+  const id = req.params.id
+
+  if (!message) {
+    res.sendStatus(400)
+  } else {
     const whisper = await getById(id)
     if (!whisper) {
-    res.sendStatus(404)
+      res.sendStatus(404)
     } else {
-    res.json(whisper)
+      await updateById(id, message)
+      res.sendStatus(200)
     }
-    })
-    app.post("/api/v1/whisper", async (req, res) => {
-      const { message } = req.body;
-      if (!message) {
-      res.sendStatus(400);
-      } else {
-      const whisper = await create(message);
-      res.status(201).json(whisper);
-      }
-      });
-
-      app.put('/api/v1/whisper/:id', async (req, res) => {
-        const { message } = req.body
-        const id = parseInt(req.params.id)
-        if(!message) {
-        res.sendStatus(400)
-        } else {
-        const whisper = await getById(id);
-        if (!whisper) {
-        res.sendStatus(404);
-        } else {
-        await updateById(id, message);
-        res.sendStatus(200);
-        }
-        }
-        })
-
+  }
+})
 
 app.delete('/api/v1/whisper/:id', async (req, res) => {
-const id = parseInt(req.params.id)
-const whisper = await getById(id)
-if(!whisper) {
-res.sendStatus(404)
-return
-}
-await deleteById(id)
-res.sendStatus(200)
+  const id = req.params.id
+  const whisper = await getById(id)
+
+  if (!whisper) {
+    res.sendStatus(404)
+    return
+  }
+
+  await deleteById(id)
+  res.sendStatus(200)
 })
 
 export { app }
